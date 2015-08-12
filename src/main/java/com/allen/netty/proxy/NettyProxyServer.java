ce0b9c0693685203fc2a2989b7304e5b81f45737
Allen <allen_pg@163.com>
2015-08-12 23:31:23 +0800

    create server

--- src/main/java/com/allen/netty/proxy/NettyProxyServer.java
+++ src/main/java/com/allen/netty/proxy/NettyProxyServer.java
@@ -1,5 +1,51 @@
 package com.allen.netty.proxy;
 
-public class NettyProxyServer {
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+import java.net.InetSocketAddress;
 
+/**
+ * Netty Server
+ * @author Allen
+ */
+public class NettyProxyServer {
+	/* Default port : 80 */
+	private int port = 80;
+	
+	public NettyProxyServer(){}
+	
+	public NettyProxyServer(int port){
+		this.port = port;
+	}
+	public void start () throws Exception {
+		EventLoopGroup bossGroup = new NioEventLoopGroup();
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
+		
+		try {
+			ServerBootstrap bootstrap = new ServerBootstrap();
+			bootstrap.group(bossGroup, workerGroup)
+			         .channel(NioServerSocketChannel.class)
+			         .localAddress(new InetSocketAddress(port))
+			         .childHandler(new ChannelInitializer<SocketChannel>(){
+
+						@Override
+						protected void initChannel(SocketChannel ch)
+								throws Exception {
+							ChannelPipeline pipeline = ch.pipeline();
+							 pipeline.addLast("codec", new HttpServerCodec());
+						}});
+			ChannelFuture future = bootstrap.bind().sync(); 
+			future.channel().closeFuture().sync();
+		} finally {
+			bossGroup.shutdownGracefully().sync();
+			workerGroup.shutdownGracefully().sync();
+		}
+	}
 }
